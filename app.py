af46e7ef6ad38d1abb8fa573cb784c93f454af5f
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-01 19:50:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'JAsuran'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://Jegaa1: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@github.com/Jegaa1/Mogenius okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://Jegaa1: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@github.com/Jegaa1/Mogenius okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
