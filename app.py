3db7dad8b576a37189d0f43f18c7305134a8633e
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-01 13:10:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'GreyMatters_Bot'
+   return 'JAsuran'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Jegaa1: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@github.com/Jegaa1/PyroFilesStoreBot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
